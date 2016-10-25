d6788ae0d35a153932bdfb80f41a9551e15bbbe0
gilson <gilson.gabriel@lais.huol.ufrn.br>
2016-10-25 08:19:21 -0300

    Inicio Ep10

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -13,13 +13,13 @@
 
         <h3>Add a new note</h3>
         <form method="POST" action="/cards/{{$card->id}}/notes">
+            {{csrf_field()}}
             <div id="form-group">
                 <textarea name="body" class="form-control"></textarea>
             </div>
             <div id="form-group">
                 <button type="submit" class="btn btn-primary">Add Note</button>
             </div>
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
         </form>
 
     </div>
